bb74063f5a92cc1576bcb59513d887cef44e3c34
cryvax <aaladan453@gmail.com>
2025-07-29 10:22:30 +0100

    setup routers

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -2,12 +2,12 @@ from fastapi import APIRouter
 
 from app.routers.auth import router as auth_router
 from app.routers.users import router as users_router
-from app.routers.product import router as product_router
-from app.routers.order import router as order_router
+from app.routers.attendance import router as attendance_router
+from app.routers.admin import router as admin_router
 
 api_router = APIRouter()
 
 api_router.include_router(auth_router, prefix="/auth", tags=["auth"])
 api_router.include_router(users_router, prefix="/users", tags=["users"])
-api_router.include_router(product_router, prefix="/product", tags=["product"])
-api_router.include_router(order_router, prefix="/order", tags=["order"])
+api_router.include_router(attendance_router, prefix="/attendance", tags=["attendance"])
+api_router.include_router(admin_router, prefix="/admin", tags=["admin"])
